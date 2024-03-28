2b8e678f8408747e67e7e4ec416254167b38c29f
tkaiman <taylorkaiman@gmail.com>
2024-03-27 19:50:16 -0600

    update to public folder

--- src/projects/components/ReviewList.tsx
+++ src/projects/components/ReviewList.tsx
@@ -50,7 +50,7 @@ function ReviewList() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          '/public/reviews.json'
+          '/reviews.json'
         );
         const data: Review[] = await response.json();
         const sortedReviews = sortReviews(data, 'rating', 'desc');
